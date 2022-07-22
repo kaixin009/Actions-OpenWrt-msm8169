012e9aad8da82aaa5369a3c93ee1659acb8db688
AK201314 <88940210+AK201314@users.noreply.github.com>
2022-07-22 21:26:18 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,3 +15,6 @@ sed -i 's/192.168.1.1/192.168.50.1/g' package/base-files/files/bin/config_genera
 
 # 安装Docker
 svn co https://github.com/NueXini/NueXini_Packages/trunk/luci-app-docker package/luci-app-docker
+
+# 安装ssr
+git clone https://github.com/fw876/helloworld package/luci-app-ssr-plus
