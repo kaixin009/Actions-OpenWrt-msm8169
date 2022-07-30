fa57055bcc03a82186501d0230b87ecd128f86b8
AK201314 <88940210+AK201314@users.noreply.github.com>
2022-07-31 01:44:57 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,3 +15,16 @@ sed -i 's/192.168.1.1/192.168.50.1/g' package/base-files/files/bin/config_genera
 
 # 安装Docker
 svn co https://github.com/NueXini/NueXini_Packages/trunk/luci-app-docker package/luci-app-docker
+
+# 删除原来的ssr
+rm -rf  feeds/luci/applications/luci-app-ssr-plus
+git clone https://github.com/fw876/helloworld package/luci-app-ssr-plus
+
+# 删除原来的argon
+rm -rf  feeds/luci/themes/luci-theme-argon
+
+# 添加老竭力的argon主题
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+
+# 修改默认主题为argon
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
