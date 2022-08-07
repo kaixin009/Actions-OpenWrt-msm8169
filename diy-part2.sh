4305cdef055dffafc4d8e9ed15ed0bd4ac7af141
AK201314 <88940210+AK201314@users.noreply.github.com>
2022-08-07 21:46:45 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -29,15 +29,5 @@ rm -rf feeds/luci/applications/luci-app-dockerman
 git clone https://github.com/lisaac/luci-app-dockerman package/luci-app-dockerman
 git clone https://github.com/lisaac/luci-lib-docker package/luci-lib-docker
 
-
-# 删除原来的argon
-rm -rf  feeds/extra/luci-theme-argon
-
-# 添加老竭力的argon主题
-git clone -b master https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
-
-# 修改默认主题为argon
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-
 # 添加cpu温度监控
 git clone https://github.com/gSpotx2f/luci-app-temp-status.git package/luci-app-temp-status
