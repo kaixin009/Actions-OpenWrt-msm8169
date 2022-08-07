a44a4f9038757f328156b74095ef5258c6d8848c
AK201314 <88940210+AK201314@users.noreply.github.com>
2022-08-07 13:33:55 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,6 +13,9 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.50.2/g' package/base-files/files/bin/config_generate
 
+# 删除原来的adbyby
+rm -rf  feeds/extra/luci-app-adbyby-plus
+#svn co https://github.com/NueXini/NueXini_Packages/trunk/luci-app-adbyby-plus package/lean/luci-app-adbyby-plus
 
 
 # 删除docker
@@ -20,19 +23,19 @@ sed -i 's/192.168.1.1/192.168.50.2/g' package/base-files/files/bin/config_genera
 #rm -rf luci-app-docker
 #popd
 
-rm -rf extra/luci-lib-docker
-rm -rf extra/luci-app-docker
-rm -rf extra/luci-app-dockerman
+rm -rf feeds/extra/luci-lib-docker
+rm -rf feeds/extra/luci-app-docker
+rm -rf feeds/extra/luci-app-dockerman
 
 # 安装Docker
 #svn co https://github.com/NueXini/NueXini_Packages/trunk/luci-app-docker package/luci-app-docker
 
-git clone https://github.com/lisaac/luci-app-dockerman package/luci-app-dockerman
-git clone https://github.com/lisaac/luci-lib-docker package/luci-lib-docker
+#git clone https://github.com/lisaac/luci-app-dockerman package/luci-app-dockerman
+#git clone https://github.com/lisaac/luci-lib-docker package/luci-lib-docker
 
 
 # 删除原来的argon
-rm -rf  extra/luci-theme-argon
+rm -rf  feeds/extra/luci-theme-argon
 
 # 添加老竭力的argon主题
 git clone -b master https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
