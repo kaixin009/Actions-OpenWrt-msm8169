829a7b475afc2cbe991534bb31dc4e2c4b07f4f6
AK201314 <88940210+AK201314@users.noreply.github.com>
2022-08-07 14:42:31 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,7 +15,7 @@ sed -i 's/192.168.1.1/192.168.50.2/g' package/base-files/files/bin/config_genera
 
 # 删除原来的adbyby
 rm -rf  feeds/extra/luci-app-adbyby-plus
-#svn co https://github.com/NueXini/NueXini_Packages/trunk/luci-app-adbyby-plus package/lean/luci-app-adbyby-plus
+svn co https://github.com/NueXini/NueXini_Packages/trunk/luci-app-adbyby-plus package/lean/luci-app-adbyby-plus
 
 
 # 删除docker
@@ -26,8 +26,8 @@ rm -rf feeds/luci/applications/luci-app-dockerman
 # 安装Docker
 #svn co https://github.com/NueXini/NueXini_Packages/trunk/luci-app-docker package/luci-app-docker
 
-#git clone https://github.com/lisaac/luci-app-dockerman package/luci-app-dockerman
-#git clone https://github.com/lisaac/luci-lib-docker package/luci-lib-docker
+git clone https://github.com/lisaac/luci-app-dockerman package/luci-app-dockerman
+git clone https://github.com/lisaac/luci-lib-docker package/luci-lib-docker
 
 
 # 删除原来的argon
