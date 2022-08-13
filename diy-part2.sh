60de2174755e596c2dc203f4c3d4690d83e2cb94
AK201314 <88940210+AK201314@users.noreply.github.com>
2022-08-14 00:28:19 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -27,15 +27,19 @@ sed -i 's/192.168.1.1/192.168.50.2/g' package/base-files/files/bin/config_genera
 # 删除docker
 #rm -rf feeds/extra/luci-lib-docker
 #rm -rf feeds/extra/luci-app-docker
+#rm -rf feeds/luci/applications/luci-app-adbyby-plus
 rm -rf feeds/luci/applications/luci-app-dockerman
 
 git clone https://github.com/shidahuilang/openwrt-package.git package/openwrt-package
+rm -rf package/openwrt-package/luci-app-ssr-plus
+git clone https://github.com/fw876/helloworld package/luci-app-ssr-plus
+git clone https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
 
 # 安装Docker
 #svn co https://github.com/NueXini/NueXini_Packages/trunk/luci-app-docker package/luci-app-docker
 
-#git clone https://github.com/lisaac/luci-app-dockerman package/luci-app-dockerman
-#git clone https://github.com/lisaac/luci-lib-docker package/luci-lib-docker
+git clone https://github.com/lisaac/luci-app-dockerman package/luci-app-dockerman
+git clone https://github.com/lisaac/luci-lib-docker package/luci-lib-docker
 
 # 添加cpu温度监控
 git clone https://github.com/AK201314/luci-app-temp-status.git package/luci-app-temp-status
