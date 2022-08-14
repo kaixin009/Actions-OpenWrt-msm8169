dd0dd588978bbf1e9aea3d1ec534fbb4eb823fde
AK201314 <88940210+AK201314@users.noreply.github.com>
2022-08-14 14:36:35 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -45,6 +45,3 @@ git clone https://github.com/lisaac/luci-lib-docker package/luci-lib-docker
 
 # 添加cpu温度监控
 git clone https://github.com/AK201314/luci-app-temp-status.git package/luci-app-temp-status
-
-cd package/wifidog-wiwiz/eqos-master-wiwiz/files/po
-mv zh-cn zh_Hans
